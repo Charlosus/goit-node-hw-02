07933ddd8c124355888061e85d5cde87b8aaa1e4
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-24 21:28:48 +0200

    Added validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,4 +1,14 @@
 const express = require("express");
+const Joi = require("joi");
+
+const contactSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    // eslint-disable-next-line no-useless-escape
+    .pattern(/^[0-9\s\-\+\(\)]+$/)
+    .required(),
+});
 
 const router = express.Router();
 const {
@@ -24,21 +34,12 @@ router.get("/:contactId", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
-  const { name, email, phone } = req.body;
-
-  if (!name) {
-    return res.status(400).json({ message: "missing required name - field" });
+  const { error } = contactSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
   }
 
-  if (!email) {
-    return res.status(400).json({ message: "missing required email - field" });
-  }
-
-  if (!phone) {
-    return res.status(400).json({ message: "missing required phone - field" });
-  }
-
-  const newContact = await addContact({ name, email, phone });
+  const newContact = await addContact(req.body);
   res.status(201).json(newContact);
 });
 
@@ -53,15 +54,19 @@ router.delete("/:contactId", async (req, res, next) => {
   res.status(200).json({ message: "contact deleted" });
 });
 
-router.put("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
+const contactUpdateSchema = Joi.object({
+  name: Joi.string().min(3),
+  email: Joi.string().email(),
+  phone: Joi.string().pattern(/^\d{9}$/),
+}).min(1);
 
-  if (!Object.keys(req.body).length) {
-    return res.status(400).json({ message: "missing fields" });
+router.put("/:contactId", async (req, res, next) => {
+  const { error } = contactUpdateSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
   }
 
-  const updated = await updateContact(contactId, req.body);
-
+  const updated = await updateContact(req.params.contactId, req.body);
   if (!updated) {
     return res.status(404).json({ message: "Not found" });
   }
