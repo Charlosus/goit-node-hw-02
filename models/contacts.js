b134dee0670d5463fe679601e9603cadf3185f41
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-24 18:21:14 +0200

    added functions

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,39 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const { v4: uuidv4 } = require('uuid');
 
-const listContacts = async () => {}
+const contactsPath = "db/contacts.json";
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(data);
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const data = await listContacts();
+  return data.find((contact) => contact.id === contactId);
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const filteredContacts = contacts.filter(
+    (contact) => contact.id !== contactId
+  );
+  const jsonData = JSON.stringify(filteredContacts, null, 2);
+  await fs.writeFile(contactsPath, jsonData);
+  return console.log(`Contact with id ${contactId} has been remove`);
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async ({name, email, phone}) => {
+  const contactId = uuidv4();
+  const newContact = { id: contactId, name, email, phone };
+
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +41,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
